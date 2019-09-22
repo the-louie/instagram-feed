2563173dc8e1e8821bd5c807470df3ace903bab3
John Brackett <hey@smashballoon.com>
2019-09-21 20:48:03 -0500

    Commented out test date

--- inc/admin/actions.php
+++ inc/admin/actions.php
@@ -539,7 +539,7 @@ function sbi_get_current_time() {
 	$current_time = time();
 
 	// where to do tests
-	$current_time = strtotime( 'November 25, 2022' ) + 1;
+	// $current_time = strtotime( 'November 25, 2022' ) + 1;
 
 	return $current_time;
 }
